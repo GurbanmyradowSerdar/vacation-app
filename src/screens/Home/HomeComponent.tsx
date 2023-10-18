0e957409e7ae4aae70f18c58f7354b50c650ff7d
Serdar <115923465+GurbanmyradowSerdar@users.noreply.github.com>
2023-10-19 00:36:05 +0500

    done the home screen need to clickable card

--- src/screens/Home/HomeComponent.tsx
+++ src/screens/Home/HomeComponent.tsx
@@ -34,109 +34,117 @@ const data: IHomeCarouselCardInterface[] = [
 
 const HomeComponentScreen = () => {
   return (
-    <View style={styles.main.wrapper}>
-      {/* Nav */}
-      <HomeScreenHorizontalWrapper style={styles.nav.container}>
-        <View style={styles.nav.content}>
-          <View style={styles.nav.avatarWrapper}>
-            <Image
-              source={require('../../assets/images/user.png')}
-              style={styles.nav.avatar}
-            />
-          </View>
-          <View style={styles.nav.textContainer}>
-            <SecondaryText
-              style={styles.nav.cheering}
-              title="Welcome Back 👋"
-            />
-            <PrimaryText style={styles.nav.name} title="Jhon Radrigaz" />
-          </View>
-        </View>
-        <TouchableOpacity activeOpacity={0.6}>
-          <BellIcon width={45} height={45} />
-        </TouchableOpacity>
-      </HomeScreenHorizontalWrapper>
-      {/* Adventure */}
-      <HomeScreenHorizontalWrapper style={styles.adventure.container}>
-        <View style={styles.adventure.darkedElement} />
-        <ImageBackground
-          source={require('../../assets/images/nature.jpg')}
-          style={styles.adventure.backgroundImage}
-          borderRadius={12}>
-          <View style={styles.adventure.contentWrapper}>
-            <View style={styles.adventure.textWrapper}>
-              <PrimaryText
-                title="Start Your Adventure"
-                style={styles.adventure.title}
-              />
-              <SecondaryText
-                title="Create adventure with your friends"
-                style={styles.adventure.subtitle}
+    <ScrollView>
+      <View style={styles.main.wrapper}>
+        {/* Nav */}
+        <HomeScreenHorizontalWrapper style={styles.nav.container}>
+          <View style={styles.nav.content}>
+            <View style={styles.nav.avatarWrapper}>
+              <Image
+                source={require('../../assets/images/user.png')}
+                style={styles.nav.avatar}
               />
             </View>
-            <TouchableOpacity
-              style={styles.adventure.button}
-              activeOpacity={0.6}>
-              <ArrowIcon
-                style={styles.adventure.buttonImage}
-                width={40}
-                height={35}
+            <View style={styles.nav.textContainer}>
+              <SecondaryText
+                style={styles.nav.cheering}
+                title="Welcome Back 👋"
               />
-            </TouchableOpacity>
+              <PrimaryText style={styles.nav.name} title="Jhon Radrigaz" />
+            </View>
           </View>
-        </ImageBackground>
-      </HomeScreenHorizontalWrapper>
-      {/* Horizontal Carousel */}
-      <View style={styles.carousel.container}>
-        <HomeScreenHorizontalWrapper style={styles.carousel.textWrapper}>
-          <PrimaryText
-            title="Recommended Place"
-            style={styles.carousel.title}
-          />
           <TouchableOpacity activeOpacity={0.6}>
-            <SecondaryText title="View All" style={styles.carousel.viewAll} />
+            <BellIcon width={45} height={45} />
           </TouchableOpacity>
         </HomeScreenHorizontalWrapper>
-        <ScrollView
-          horizontal
-          style={styles.carousel.carouselWrapper}
-          showsHorizontalScrollIndicator={false}>
-          {data.map((item, index) => {
-            return (
-              <HomeCarouselCardComponent
-                key={index}
-                img={item.img}
-                rating={item.rating}
-                subtitle={item.subtitle}
-                title={item.title}
+        {/* Adventure */}
+        <HomeScreenHorizontalWrapper style={styles.adventure.container}>
+          <View style={styles.adventure.darkedElement} />
+          <ImageBackground
+            source={require('../../assets/images/nature.jpg')}
+            style={styles.adventure.backgroundImage}
+            borderRadius={12}>
+            <View style={styles.adventure.contentWrapper}>
+              <View style={styles.adventure.textWrapper}>
+                <PrimaryText
+                  title="Start Your Adventure"
+                  style={styles.adventure.title}
+                />
+                <SecondaryText
+                  title="Create adventure with your friends"
+                  style={styles.adventure.subtitle}
+                />
+              </View>
+              <TouchableOpacity
+                style={styles.adventure.button}
+                activeOpacity={0.6}>
+                <ArrowIcon
+                  style={styles.adventure.buttonImage}
+                  width={40}
+                  height={35}
+                />
+              </TouchableOpacity>
+            </View>
+          </ImageBackground>
+        </HomeScreenHorizontalWrapper>
+        {/* Horizontal Carousel */}
+        <View style={styles.carousel.container}>
+          <HomeScreenHorizontalWrapper style={styles.carousel.textWrapper}>
+            <PrimaryText
+              title="Recommended Place"
+              style={styles.carousel.title}
+            />
+            <TouchableOpacity activeOpacity={0.6}>
+              <SecondaryText title="View All" style={styles.carousel.viewAll} />
+            </TouchableOpacity>
+          </HomeScreenHorizontalWrapper>
+          <ScrollView
+            horizontal
+            style={styles.carousel.carouselWrapper}
+            showsHorizontalScrollIndicator={false}>
+            {data.map((item, index) => {
+              return (
+                <HomeCarouselCardComponent
+                  key={index}
+                  img={item.img}
+                  rating={item.rating}
+                  subtitle={item.subtitle}
+                  title={item.title}
+                />
+              );
+            })}
+          </ScrollView>
+        </View>
+        {/* Friends */}
+        <HomeScreenHorizontalWrapper style={styles.friends.container}>
+          <View style={styles.friends.textWrapper}>
+            <PrimaryText title="Your Friends" style={styles.friends.title} />
+            <Text style={styles.friends.subtitleSpan}>
+              <SecondaryText title="You have" style={styles.friends.subtitle} />{' '}
+              23{' '}
+              <SecondaryText title="friends" style={styles.friends.subtitle} />
+            </Text>
+          </View>
+          <View style={styles.friends.avatarsWrapper}>
+            <View style={styles.friends.avatarWrapper}>
+              <Image
+                source={require('../../assets/images/friend-1.png')}
+                style={styles.friends.avatar}
               />
-            );
-          })}
-        </ScrollView>
+            </View>
+            <View style={styles.friends.avatarWrapper1}>
+              <Image
+                source={require('../../assets/images/friend.png')}
+                style={styles.friends.avatar}
+              />
+            </View>
+            <View style={styles.friends.avatarNumbersWrapper}>
+              <PrimaryText title="21+" style={styles.friends.avatarNumbers} />
+            </View>
+          </View>
+        </HomeScreenHorizontalWrapper>
       </View>
-      {/* Friends */}
-      {/* <HomeScreenHorizontalWrapper>
-        <View>
-          <PrimaryText title="Your Friends" />
-          <Text>
-            <SecondaryText title="You have" />
-            23
-            <SecondaryText title="friends" />
-          </Text>
-        </View>
-        <View>
-          <View>
-            <Image source={require('../../assets/images/friend-1.png')} />
-          </View>
-          <View>
-            <Image source={require('../../assets/images/friend.png')} />
-          </View>
-          <View>
-            <PrimaryText title="21+" />
-          </View>
-        </View>
-      </HomeScreenHorizontalWrapper> */}
-    </View>
+    </ScrollView>
   );
 };
 
@@ -274,6 +282,76 @@ const styles = {
     },
     carouselWrapper: {
       marginVertical: -20,
+    },
+  }),
+  friends: StyleSheet.create({
+    container: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      flexDirection: 'row',
+      marginBottom: 60,
+    },
+    textWrapper: {
+      gap: 4,
+    },
+    title: {
+      fontSize: 20,
+    },
+    subtitle: {
+      fontSize: 14,
+    },
+    subtitleSpan: {
+      fontSize: 14,
+      color: '#FFC15F',
+      fontFamily: 'IBMPlexSans-Regular',
+    },
+    avatarsWrapper: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      position: 'relative',
+    },
+    avatarWrapper: {
+      backgroundColor: '#3AAAA7',
+      borderWidth: 2,
+      borderColor: '#fff',
+      borderRadius: 100,
+      overflow: 'hidden',
+      position: 'absolute',
+      right: 44,
+      top: 0,
+    },
+    avatarWrapper1: {
+      backgroundColor: '#3AAAA7',
+      borderWidth: 2,
+      borderColor: '#fff',
+      borderRadius: 100,
+      overflow: 'hidden',
+      position: 'absolute',
+      right: 22,
+      top: 0,
+    },
+    avatar: {
+      width: 36,
+      height: 36,
+      objectFit: 'contain',
+    },
+    avatarNumbersWrapper: {
+      backgroundColor: '#FFC15F',
+      width: 36,
+      height: 36,
+      objectFit: 'contain',
+      borderWidth: 2,
+      borderColor: '#fff',
+      borderRadius: 100,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    avatarNumbers: {
+      fontSize: 14,
+      fontFamily: 'IBMPlexSans-BoldItalic',
     },
   }),
 };
